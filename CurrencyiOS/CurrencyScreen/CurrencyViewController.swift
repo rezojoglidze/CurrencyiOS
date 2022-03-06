2b8ca0db091c2e5330593c0cee3a5d55d5dda3f2
Rezo Joglidze <rezojoglidze7@gmail.com>
2022-03-06 11:52:34 +0400

    added currency choose logics

--- CurrencyiOS/CurrencyScreen/CurrencyViewController.swift
+++ CurrencyiOS/CurrencyScreen/CurrencyViewController.swift
@@ -12,18 +12,21 @@ protocol CurrencyViewProtocol: AnyObject {
 }
 
 class CurrencyViewController: UIViewController {
-
-    @IBOutlet weak var sdaddasdsadsa: UITextField!
     
     @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var submitBtn: UIButton!
     @IBOutlet private weak var myBalanceCollectionView: UICollectionView!
-    @IBOutlet private weak var amountConvertView: AmountConvertView!
-    
-    private let pickerView: UIPickerView = UIPickerView()
+    @IBOutlet private weak var BoughtCurrencyValue: UILabel!
+    @IBOutlet private weak var sellCurrencyAmountTextField: UITextField!
+    @IBOutlet private weak var sellButton: UIButton!
+    @IBOutlet private weak var BuyButton: UIButton!
+
+    private var pickerView: UIPickerView = UIPickerView()
+    private var toolBar = UIToolbar()
+
     var viewModel: CurrencyViewModelProtocol!
-    var toolBar = UIToolbar()
-    var picker  = UIPickerView()
+    private var isSell: Bool = false
+    
     static func instantiate() -> CurrencyViewController {
         let storyBoard = UIStoryboard(name: "Currency", bundle: nil)
         let viewController = storyBoard.instantiateViewController(withIdentifier: "CurrencyViewController") as? CurrencyViewController ?? .init()
@@ -46,43 +49,44 @@ class CurrencyViewController: UIViewController {
         removeKeyboardObservers()
     }
     
+    
+    @IBAction func sellCurrencyChooseBtnTapped(_ sender: Any) {
+        isSell = true
+        showPickerView()
+    }
+    
+    @IBAction func buyCurrencyChooseBtnTapped(_ sender: Any) {
+        isSell = false
+        showPickerView()
+    }
+    
+    
     private func setupView() {
         submitBtn.setTitle("sumbit".uppercased(), for: .normal)
         submitBtn.layer.cornerRadius = 24
-        amountConvertView.delegate = self
     }
     
-    private func setupPickerView() {
+    private func showPickerView() {
+        pickerView = UIPickerView.init()
         pickerView.delegate = self
         pickerView.dataSource = self
-        let toolBar = UIToolbar()
-        toolBar.barStyle = UIBarStyle.default
-        toolBar.tintColor = .black
-        toolBar.sizeToFit()
-        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(doneClick))
-        toolBar.setItems([flexibleSpace, doneButton], animated: false)
-        toolBar.isUserInteractionEnabled = true
-        sdaddasdsadsa.inputView = pickerView
-        sdaddasdsadsa.inputAccessoryView = toolBar
+        pickerView.backgroundColor = UIColor.white
+        pickerView.setValue(UIColor.black, forKey: "textColor")
+        pickerView.autoresizingMask = .flexibleWidth
+        pickerView.contentMode = .center
+        pickerView.frame = CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
+           self.view.addSubview(pickerView)
+                   
+        toolBar = UIToolbar.init(frame: CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 50))
+           toolBar.items = [UIBarButtonItem.init(title: "Done", style: .done, target: self, action: #selector(onDoneButtonTapped))]
+           self.view.addSubview(toolBar)
     }
     
-    
-    @objc func doneClick() {
-        print("dsada")
-//        curTextfield?.resignFirstResponder()
-//        if let section = tableView.headerView(forSection: .zero) as? StatementHeaderViewCell {
-//            if curTextfield == toDateTextField {
-//                section.changeDate(toDate: toDate ?? Date())
-//            } else {
-//                section.changeDate(fromDate: fromDate ?? Date())
-//            }
-//            loadNewTransactions()
-//            fromdatePicker.maximumDate = toDate
-//            todatePicker.minimumDate = fromDate
-//        }
+    @objc func onDoneButtonTapped() {
+        toolBar.removeFromSuperview()
+        pickerView.removeFromSuperview()
     }
-    
+
     private func setupCollectionView() {
         myBalanceCollectionView.register(UINib(nibName: String(describing: MyBalancesCell.self), bundle: nil), forCellWithReuseIdentifier: String(describing: MyBalancesCell.self))
         myBalanceCollectionView.dataSource = self
@@ -124,11 +128,19 @@ extension CurrencyViewController: UIPickerViewDelegate, UIPickerViewDataSource {
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return 5
+        return viewModel.pickerViewNumberOfRowsInComponent(isSell: isSell)
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return "21213132"
+        return isSell ? viewModel.currentAvailableSellCurrencies[row].rawValue : viewModel.currentAvailableBuyCurrencies[row].rawValue
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurr =  isSell ? viewModel.currentAvailableSellCurrencies[row] : viewModel.currentAvailableBuyCurrencies[row]
+        
+        let btn = isSell ? sellButton : BuyButton
+        isSell ? (viewModel.sellCurrentCurrency = selectedCurr) : (viewModel.buyCurrencCurrency = selectedCurr)
+        btn?.setTitle(selectedCurr.rawValue.uppercased(), for: .normal)
     }
 }
 
@@ -136,34 +148,9 @@ extension CurrencyViewController: CurrencyViewProtocol {
     
 }
 
-extension CurrencyViewController: AmountConvertViewDelegate {
-    func didTappedCurrencyChooseBtn(isSell: Bool) {
-      
-        picker = UIPickerView.init()
-            picker.delegate = self
-            picker.dataSource = self
-            picker.backgroundColor = UIColor.white
-            picker.setValue(UIColor.black, forKey: "textColor")
-            picker.autoresizingMask = .flexibleWidth
-            picker.contentMode = .center
-            picker.frame = CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 300)
-            self.view.addSubview(picker)
-                    
-            toolBar = UIToolbar.init(frame: CGRect.init(x: 0.0, y: UIScreen.main.bounds.size.height - 300, width: UIScreen.main.bounds.size.width, height: 50))
-            toolBar.barStyle = .blackTranslucent
-            toolBar.items = [UIBarButtonItem.init(title: "Done", style: .done, target: self, action: #selector(onDoneButtonTapped))]
-            self.view.addSubview(toolBar)
-    }
-    
-    @objc func onDoneButtonTapped() {
-        toolBar.removeFromSuperview()
-        picker.removeFromSuperview()
-    }
-}
-
 extension CurrencyViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return viewModel.numberOfItemsInSection
+        return viewModel.collectionViewNumberOfItemsInSection
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
--- CurrencyiOS/CurrencyScreen/CurrencyViewModel.swift
+++ CurrencyiOS/CurrencyScreen/CurrencyViewModel.swift
@@ -7,39 +7,68 @@
 
 import Foundation
 
-enum availableCurrencies: String, CaseIterable {
+enum AvailableCurrencies: String, CaseIterable {
     case eur
     case usd
     case jpy
 }
 
 protocol CurrencyViewModelProtocol: AnyObject {
-    var numberOfItemsInSection: Int { get }
+    var collectionViewNumberOfItemsInSection: Int { get }
+    
+    var sellCurrentCurrency: AvailableCurrencies { get set }
+    var buyCurrencCurrency: AvailableCurrencies { get set }
+    var currentAvailableBuyCurrencies: [AvailableCurrencies] { get }
+    var currentAvailableSellCurrencies: [AvailableCurrencies] { get }
+    func pickerViewNumberOfRowsInComponent(isSell: Bool) -> Int
     func getMyBalanceInfo(with index: Int) -> (Double, String)
+    
 }
 
 class CurrencyViewModel {
     
     weak var view: CurrencyViewProtocol?
-    private let userBalance: [availableCurrencies: Double] = [
-        availableCurrencies.eur: 1000,
-        availableCurrencies.usd: 0,
-        availableCurrencies.jpy: 0
-    ]
     
+    private let userBalance: [AvailableCurrencies: Double] = [
+        AvailableCurrencies.eur: 1000,
+        AvailableCurrencies.usd: 0,
+        AvailableCurrencies.jpy: 0
+    ]
+    var sellCurrentCurrency: AvailableCurrencies = .eur
+    var buyCurrencCurrency: AvailableCurrencies = .usd
+    var currentAvailableBuyCurrencies: [AvailableCurrencies] = []
+    var currentAvailableSellCurrencies: [AvailableCurrencies] = []
     init(view: CurrencyViewProtocol) {
         self.view = view
     }
 }
 
 extension CurrencyViewModel: CurrencyViewModelProtocol {
+    func pickerViewNumberOfRowsInComponent(isSell: Bool) -> Int {
+        let curr = isSell ? sellCurrentCurrency : buyCurrencCurrency
+        let currencies =  AvailableCurrencies.allCases.filter { currency in
+            return currency != curr
+        }
+        filterCurrentAvailableCurrrencies(isSell: isSell)
+        return currencies.count
+    }
+    
+    private func filterCurrentAvailableCurrrencies(isSell: Bool) {
+        let FilteringCurrency = isSell ? sellCurrentCurrency: buyCurrencCurrency
+        let currencies =  AvailableCurrencies.allCases.filter { currency in
+            return currency != FilteringCurrency
+        }
+        
+        isSell ? (currentAvailableSellCurrencies = currencies) : ( currentAvailableBuyCurrencies = currencies)
+    }
+    
     func getMyBalanceInfo(with index: Int) -> (Double, String) {
-        let currency = availableCurrencies.allCases[index]
+        let currency = AvailableCurrencies.allCases[index]
         let balance = userBalance[currency]
         return (balance ?? 0, currency.rawValue)
     }
     
-    var numberOfItemsInSection: Int {
-        return availableCurrencies.allCases.count
+    var collectionViewNumberOfItemsInSection: Int {
+        return AvailableCurrencies.allCases.count
     }
 }
